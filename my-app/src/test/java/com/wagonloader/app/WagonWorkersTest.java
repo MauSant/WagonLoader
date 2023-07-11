345fca0600c97233687e6c1d6a8612814886b147
maucsant <maubr93@gmail.com>
2023-07-11 14:08:57 -0300

    feat: Add test for wagonWorker::find

--- my-app/src/test/java/com/wagonloader/app/WagonWorkersTest.java
+++ my-app/src/test/java/com/wagonloader/app/WagonWorkersTest.java
@@ -1,5 +1,8 @@
 package com.wagonloader.app;
 
+
+import static org.mockito.Mockito.mockStatic;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
@@ -9,6 +12,9 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
 import java.util.logging.Logger;
 
 import com.fasterxml.jackson.core.JsonParseException;
@@ -18,14 +24,14 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-public class WagonLoaderTest {
+public class WagonWorkersTest {
     ObjectMapper mapper = new ObjectMapper();
-    WagonLoader wagonLoader = new WagonLoader();
+    WagonWorkers wagonWorkers = new WagonWorkers();
     JsonNode wagonData = readJson("wagonData.json");
-    private static final Logger logger = Logger.getLogger(WagonLoaderTest.class.getName());
+    private static final Logger logger = Logger.getLogger(WagonWorkersTest.class.getName());
 
     public JsonNode readJson(String fileName){
-        InputStream stream = WagonLoaderTest.class.getResourceAsStream("../../../"+fileName);
+        InputStream stream = WagonWorkersTest.class.getResourceAsStream("../../../"+fileName);
         try {
             return mapper.readValue(stream, JsonNode.class);
         } catch (JsonParseException e) {
@@ -49,30 +55,22 @@ public class WagonLoaderTest {
         return null;
     }
 
+  
     @ParameterizedTest
     @ValueSource(strings = {
-        "WagonLoaderTest/simpleKey.json",
-        "WagonLoaderTest/simpleList.json",
-        "WagonLoaderTest/findSimpleKey.json",
-        "WagonLoaderTest/findNestedObj.json",
-        "WagonLoaderTest/findNestedList.json"
+        "WagonWorkersTest/find/oneNest.json",
+        "WagonWorkersTest/find/twoNest.json",
     })
-    public void testFillPayloadOutput(String inputFileName) throws IOException{
+    public void testingFind(String inputFileName){
         JsonNode input = readJson(inputFileName);
-        JsonNode targetJson = input.get("target");
+        Object params = input.get("params").asText();
+        String workerName =  "FIND";
         JsonNode expected = input.get("expected");
-
-        Map<String, Object> map = new HashMap<>();
-        Object r = wagonLoader.fillPayload("", targetJson, map, wagonData );
-        JsonNode actual = mapper.convertValue(r, JsonNode.class);
-        logger.info("["+inputFileName+"]"+prettyPrintObject(actual));
+        Object actual = WagonWorkers.callWorker(workerName, params, wagonData);
+        logger.info("actual :: "+ actual);
         Assert.assertEquals("msg", expected, actual); 
-
     }
 
-    //TODO
-    // @Test
-    // public void testEvaluate(){
-    //     wagonLoader.evaluate("FIND(key1,key2)");
-    // }
+
+
 }
