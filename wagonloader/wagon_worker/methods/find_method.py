40564aca7103908872186567735b112d4e112f06
Mauricio Santiago <maubr93@gmail.com>
2024-02-12 14:18:25 -0300

    feat: Add try catch to method find

--- wagonloader/wagon_worker/methods/find_method.py
+++ wagonloader/wagon_worker/methods/find_method.py
@@ -2,17 +2,22 @@ import functools
 import operator
 from typing import Sequence
 
-def wagon_get_item(sequence:Sequence, index:str):
+def wagon_get_item(sequence:Sequence, index:str) -> dict|list|str:
     try:
         if isinstance(sequence,list):
             index = int(index)
-    except Exception as e:
-        print("index is not a integer!")
+    except ValueError as e:
+        raise ValueError(f"Error when trying to access list index with the string `{index}`.")
 
-    return operator.getitem(sequence, index)
+    try:
+        return operator.getitem(sequence, index)
+    except IndexError as e:
+        raise ValueError(f"The path does not exist in the data. index `{index}` has issues")
+    except KeyError as e:
+        raise ValueError(f"The path does not exist in the data. key `{index}` has issues")
 
 
-def find(params:list[str], wagon_data:dict|list|str) -> str:
+def find(params:list[str], wagon_data:dict|list|str) -> dict|list|str:
     if len(params) > 1:
         raise ValueError("Too many parameters for method FIND")
     elif len(params) == 0:
@@ -20,5 +25,5 @@ def find(params:list[str], wagon_data:dict|list|str) -> str:
     
     str(params[0])
 
-    return functools.reduce(wagon_get_item, params[0], wagon_data)
+    return functools.reduce(wagon_get_item, params[0].split("."), wagon_data)
 
